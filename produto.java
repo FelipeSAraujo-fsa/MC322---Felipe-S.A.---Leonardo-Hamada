d9996a44d6435230775aa78752bd36bc289ad622
Leonardo Tetsuo <cleberkuyama@gmail.com>
2026-09-02 21:17:39 -0300

    xcaxc

--- produto.java
+++ produto.java
@@ -1,56 +1,36 @@
-public class produto {
-    private String id;
-    private String nome;
-    private String status;
-    private int quantidadeMateriaPrimaNecessaria;
-    private int quantProd;
-    private int quantTotal;
+public class Produto {
 
-    public produto(String id, String nome, int quantidadeMateriaPrimaNecessaria,int quantProd) {
-        this.id = id;
-        this.nome = nome;
-        this.quantidadeMateriaPrimaNecessaria = quantidadeMateriaPrimaNecessaria;
-        this.status = "Aguardando processamento";
-        this.quantProd= quantProd;
-        this.quantTotal = 0;
+    private String idProd;
+    private String nomeProd;
+    private String statProd;
+    private double quantMatPrimaNesc;
+
+    public Produto (String idProd, String nomeProd, String statProd){
+
+        this.idProd = idProd;
+        this.nomeProd = nomeProd;
+        this.statProd = statProd;
+        this.quantMatPrimaNesc = 0.0;
     }
 
-    public void processar(int quantProduzida) {
-        status = "Processado";
-        quantProd = quantProduzida;
-        quantTotal += quantProduzida;
-        System.out.println("[OK] O produto " + nome + " foi processado");
+    public getDemandaMatPrima (){
+        return quantMatPrimaNesc;
     }
 
-    public void inspecionarProd() {
-        this.status = "Inspecionado";
+    public getIdP(){
+        return idProd;
     }
 
-    public void definirDemandaMateriaPrima(int demanda) {
-        quantidadeMateriaPrimaNecessaria = demanda;
-
-        System.out.println("[OK] A demanda de materia prima do produto "  + nome  + " foi definida como " + demanda);
+    public getNomeP(){
+        return nomeProd;
     }
 
-    public int getDemandaMateriaPrima() {
-        return quantidadeMateriaPrimaNecessaria;
+    public getStatP(){
+        return nomeProd;
     }
 
-    public String getId() {
-        return id;
+    public definirQuantMatPrima(){
+        
     }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-    public int getquantProd() {
-        return quantProd;
-    }
-    public int getquantTotal() {
-        return quantTotal;
-    }
+    
 }
